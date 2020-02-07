a991c0860b38303f8fe7e3b656cd6b714afd90da
BingQin <qinbing08@gmail.com>
2020-02-07 10:49:57 +0800

    实现基本的算法
    这个提交中实现了基本的爬虫算法
    虽然它还很原始，但是我想要小步提交。

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,17 +6,94 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        //待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        //已经处理的链接池
+        Set<String> processdLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
+            //ArrayList从尾部删除更有效率
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processdLinks.contains(link)) {
+                continue;
+            }
+
+            //我们只关心news.sina的，排除登录页面
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag->aTag.attr("href")).forEach(linkPool::add);
+                //假如这是一个新闻的详情页面，就存入数据库，否则就什么都不做
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                processdLinks.add(link);
+            } else {
+                //这是我们不感兴趣的，不处理它
+            }
+        }
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                System.out.println("");
+                String title = articleTags.get(0).child(0).text();
+                System.out.println("title = " + title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        //这是我们感兴趣的，我们只处理新浪站内的链接
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
+
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+            System.out.println(link);
+        }
+
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36");
+
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity1));
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
         }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
